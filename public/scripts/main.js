8ce14179853d7f2ceb18c59f00f359c527997332
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-09-06 16:02:22 -0300

    Aula 4

---
+++ public/scripts/main.js
@@ -0,0 +1,13 @@
+import Modal from './modal.js'
+
+const modal = Modal()
+
+
+const checkButtons = document.querySelectorAll(".actions a.check")
+
+checkButtons.forEach(button =>{
+    button.addEventListener("click", event =>{
+        modal.open()
+    })
+})
+
